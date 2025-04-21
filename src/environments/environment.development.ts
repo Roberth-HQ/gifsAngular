61f86f7b9f568b9c407ea025025da32bb562587c
Roberth-HQ <rocahuqui@gmail.com>
2025-04-20 23:33:14 -0400

    pestaña trending gifs completdada

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,9 +3,10 @@ export const environment = {
     production:true,
     companyName: 'Gifs',
     companyName2:'App',
-    companySlogan:'Maneja Tus Gifs'
+    companySlogan:'Maneja Tus Gifs',
 
     //api keys
-
+    giphyApiKey:'RqzpMaBbQoGSZYDkTHfwHL2VPAnwRt7w',
+    giphyUrl:'https://api.giphy.com/v1'
     //Urls Rederencia
 };
